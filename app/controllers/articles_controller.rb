e5722735ed7a115ff2170c2d5715892e37a94d14
zhangwei <zhangwei.unionpaysmart.com>
2015-08-28 17:55:44 +0800

    dev

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,19 +4,26 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
     @article = Article.find(params[:id])
   end
 
-  def delete
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
